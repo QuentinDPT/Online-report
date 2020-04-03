aa305f3bec3c4fa4370c2b9a82b7d9d507bc8434
QuentinDPT <quentin@depotter.fr>
2020-04-03 14:17:01 +0200

    fix view bug

--- Views/a-user.php
+++ Views/a-user.php
@@ -25,65 +25,62 @@ $SkillsUnacq = SkillAcquireController::getUnacquiredSkillFromStudentID($Usr->id)
     <div class="container-lg">
       <h2>Mes réussites</h2>
       <div class="row overflow-auto">
-        <h2>Mes réussites</h2>
-        <div class="row overflow-auto">
-          <?php
-          if($Skills == null || count($Skills) == 0){
-          ?>
-          <p>Pas encore de réussites</p>
-          <?php
-          }else{
-            foreach ($Skills as $i) {
-          ?>
-          <div class="col-md-4 col-sm-12 text-center patent-container">
-            <img src="<?= $i->image ?>">
-            <p><?= $i->name ?></p>
-            <p><?= $i->note . " " . $i->status ?></p>
-          </div>
-          <?php
-            }
-          }
-          ?>
+        <?php
+        if($Skills == null || count($Skills) == 0){
+        ?>
+        <p>Pas encore de réussites</p>
+        <?php
+        }else{
+          foreach ($Skills as $i) {
+        ?>
+        <div class="col-md-4 col-sm-12 text-center patent-container">
+          <img src="<?= $i->image ?>">
+          <p><?= $i->name ?></p>
+          <p><?= $i->note . " " . $i->status ?></p>
         </div>
-        <h2>Réussites en cours</h2>
-        <div class="row overflow-auto">
-          <?php
-          if($SkillsObs == null || count($SkillsObs) == 0){
-          ?>
-          <p>Pas de réussites en cours</p>
-          <?php
-          }else{
-            foreach ($SkillsObs as $i) {
-          ?>
-          <div class="col-md-4 col-sm-12 text-center patent-container">
-            <img src="<?= $i->image ?>">
-            <p><?= $i->name ?></p>
-            <p><?= $i->note . " " . $i->status ?></p>
-          </div>
-          <?php
-            }
+        <?php
           }
-          ?>
+        }
+        ?>
+      </div>
+      <h2>Réussites en cours</h2>
+      <div class="row overflow-auto">
+        <?php
+        if($SkillsObs == null || count($SkillsObs) == 0){
+        ?>
+        <p>Pas de réussites en cours</p>
+        <?php
+        }else{
+          foreach ($SkillsObs as $i) {
+        ?>
+        <div class="col-md-4 col-sm-12 text-center patent-container">
+          <img src="<?= $i->image ?>">
+          <p><?= $i->name ?></p>
+          <p><?= $i->note . " " . $i->status ?></p>
         </div>
-        <h2>Donner une réussite</h2>
-        <div class="row overflow-auto">
-          <?php
-          if($SkillsUnacq == null || count($SkillsUnacq) == 0){
-          ?>
-          <p>Plus de réussites à donner</p>
-          <?php
-          }else{
-            foreach ($SkillsUnacq as $i) {
-          ?>
-          <div class="col-md-4 col-sm-12 text-center">
-            <img src="<?= $i->image ?>">
-            <p><?= $i->name ?></p>
-          </div>
-          <?php
-            }
+        <?php
           }
-          ?>
+        }
+        ?>
+      </div>
+      <h2>Donner une réussite</h2>
+      <div class="row overflow-auto">
+        <?php
+        if($SkillsUnacq == null || count($SkillsUnacq) == 0){
+        ?>
+        <p>Plus de réussites à donner</p>
+        <?php
+        }else{
+          foreach ($SkillsUnacq as $i) {
+        ?>
+        <div class="col-md-4 col-sm-12 text-center">
+          <img src="<?= $i->image ?>">
+          <p><?= $i->name ?></p>
         </div>
+        <?php
+          }
+        }
+        ?>
       </div>
     </div>
   </body>
