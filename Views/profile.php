0c7722b175796b26b233b0ac8ba0e9177c388e2c
QuentinDPT <quentin@depotter.fr>
2020-04-11 17:39:31 +0200

    UPDATE report an issue, suggest updates

--- Views/profile.php
+++ Views/profile.php
@@ -78,7 +78,11 @@ if($User == null){
 
 
       <h3>Droits</h3>
-      <input type="button" class="btn btn-danger ml-2" name="" value="Supprimer mon compte">
+      <div class="d-flex flex-column ml-2 p-0 col-3">
+        <input type="button" class="btn btn-secondary" name="" value="Proposer des modifications" onclick="window.open('https://github.com/QuentinDPT/Online-report/issues','_blank');">
+        <input type="button" class="btn btn-warning mt-2" name="" value="Signaler un problème" onclick="window.open('https://github.com/QuentinDPT/Online-report/issues/new','_blank');">
+        <input type="button" class="btn btn-danger mt-2" name="" value="Supprimer mon compte">
+      </div>
     </div>
 
   </body>
