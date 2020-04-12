a03c0fa7dcdfcb5c5dfa838948673567afdf155f
QuentinDPT <quentin@depotter.fr>
2020-04-12 11:26:31 +0200

    UPDATE Social media

--- Views/Common/header.php
+++ Views/Common/header.php
@@ -4,6 +4,13 @@
 <title><?php echo $ApplicationName . ($PageName == "" ? "" : " - " . $PageName) ?></title>
 <meta name="theme-color" content="#343A40">
 
+<meta property="og:title" content="<?php echo $ApplicationName ?>">
+<meta property="og:description" content="Système de notation des classes maternelles">
+<meta property="og:image" content="http://laclasse.depotter.fr/src/img/social.png">
+<meta property="og:url" content="http://laclasse.depotter.fr/connexion">
+
+<meta property="og:site_name" content="European Travel, Inc.">
+
 <link rel="stylesheet" href="/src/lib/bootstrap-4.4.1-dist/css/bootstrap.min.css">
 
 <script src="/src/lib/JQuery-3.4.1/JQuery.min.js"></script>
